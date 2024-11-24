2e557c64ba748559a5e62a7617ceb016bf7ac5f5
ykhedar <yogeshkhedar@gmail.com>
2024-11-24 14:00:57 +0100

    modified UI as per requirement

--- src/routes/welcome.ts
+++ src/routes/welcome.ts
@@ -11,7 +11,7 @@ import { CardGradient, Typography } from "@ory/elements-markup"
 
 export const createWelcomeRoute: RouteCreator =
   (createHelpers) => async (req, res) => {
-    res.locals.projectName = "Welcome to Ory"
+    res.locals.projectName = "Welcome to AutrikOS"
 
     const { frontend } = createHelpers(req, res)
     const session = req.session
@@ -37,52 +37,58 @@ export const createWelcomeRoute: RouteCreator =
         selectedLink: "welcome",
       }),
       projectInfoText: Typography({
-        children: `Your Ory Account Experience is running at ${req.header(
-          "host",
-        )}.`,
+        children: `You are on the AutrikOS platform.`,
         type: "regular",
         size: "small",
         color: "foregroundMuted",
       }),
       concepts: [
+        // CardGradient({
+        //   heading: "Data Visualisation",
+        //   content:
+        //     "Browse through all point clouds uploaded on the server.",
+        //   action:
+        //     "https://autrik.com/api/data/",
+        //   target: "_blank",
+        // }),
         CardGradient({
-          heading: "Getting Started",
+          heading: "Video Streaming",
           content:
-            "Jump start your project and complete the quickstart tutorial to get a broader overview of Ory Network.",
+            "Share your device's Video Stream to the AutrikOS Dashboard.",
           action:
-            "https://www.ory.sh/docs/getting-started/integrate-auth/expressjs",
+            "https://videoshare.autrik.com/",
           target: "_blank",
         }),
-        CardGradient({
-          heading: "User flows",
-          content:
-            "Implement flows that users perform themselves as opposed to administrative intervention.",
-          action: "https://www.ory.sh/docs/kratos/self-service",
-          target: "_blank",
-        }),
-        CardGradient({
-          heading: "Identities 101",
-          content:
-            "Every identity can have its own model - get to know the ins and outs of Identity Schemas.",
-          action:
-            "https://www.ory.sh/docs/kratos/manage-identities/identity-schema",
-          target: "_blank",
-        }),
-        CardGradient({
-          heading: "Sessions",
-          content:
-            "Ory Network manages sessions for you - get to know how sessions work.",
-          action: "https://www.ory.sh/docs/kratos/session-management/overview",
-          target: "_blank",
-        }),
-        CardGradient({
-          heading: "Custom UI",
-          content:
-            "Implementing these pages in your language and framework of choice is straightforward using our SDKs.",
-          action:
-            "https://www.ory.sh/docs/kratos/bring-your-own-ui/configure-ory-to-use-your-ui",
-          target: "_blank",
-        }),
+        // CardGradient({
+        //   heading: "User flows",
+        //   content:
+        //     "Implement flows that users perform themselves as opposed to administrative intervention.",
+        //   action: "https://www.ory.sh/docs/kratos/self-service",
+        //   target: "_blank",
+        // }),
+        // CardGradient({
+        //   heading: "Identities 101",
+        //   content:
+        //     "Every identity can have its own model - get to know the ins and outs of Identity Schemas.",
+        //   action:
+        //     "https://www.ory.sh/docs/kratos/manage-identities/identity-schema",
+        //   target: "_blank",
+        // }),
+        // CardGradient({
+        //   heading: "Sessions",
+        //   content:
+        //     "Ory Network manages sessions for you - get to know how sessions work.",
+        //   action: "https://www.ory.sh/docs/kratos/session-management/overview",
+        //   target: "_blank",
+        // }),
+        // CardGradient({
+        //   heading: "Custom UI",
+        //   content:
+        //     "Implementing these pages in your language and framework of choice is straightforward using our SDKs.",
+        //   action:
+        //     "https://www.ory.sh/docs/kratos/bring-your-own-ui/configure-ory-to-use-your-ui",
+        //   target: "_blank",
+        // }),
       ].join("\n"),
     })
   }
